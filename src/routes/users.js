38534a9c149ccbea27f8d3bd2530738a8ccad989
Carlos del Blanco <delblanco.carlos@gmail.com>
2023-02-20 23:47:30 +0100

    Add users routes

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,4 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/signin", (req, res) => {
+  res.send("Signin in");
+});
+
+router.get("/signup", (req, res) => {
+  res.send("Authentication form");
+});
+
 module.exports = router;
